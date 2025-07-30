4b07c37abb2ff46f10e62aafdb3a0999236561e9
YesidLM12 <yesidlondono342@gmail.com>
2025-07-29 20:28:43 -0500

    Solución al problema que la navbar no era responsive.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,12 +2,15 @@ import styles from "../styles/Header.module.css";
 import NavBar from "./navbar";
 import ArrowDown from "../assets/images/icon-arrow-down.svg";
 import HamburgerMenu from "./HamburguerMenu";
+import { useMediaQuery } from "react-responsive";
 
 export default function Header() {
+  const isMobile = useMediaQuery({query:"(max-width:768px)"});
   return (
     <div className={styles.containerHeader}>
-      <HamburgerMenu />
-      <NavBar />
+      {isMobile ? <HamburgerMenu /> : <NavBar />}
+      
+      
       <div className={styles.contentHeader}>
         <h1 className={styles.title}>WE ARE CREEATIVES</h1>
         <img src={ArrowDown} alt="arrow" />
